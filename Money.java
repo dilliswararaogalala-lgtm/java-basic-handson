452734c82ecafccece04eb656a2ba1ec02f55500
Dilliswararao Galala <dilliswararaogalala@gmail.com>
2026-04-27 15:23:51 +0530

    Adds Money class and StringUtils class

--- Money.java
+++ Money.java
@@ -1,1 +1,19 @@
 // Money.java
+
+class Money {
+  int value;
+
+  Money(int value){
+    this.value = value;
+  }
+
+  public Money add(Money money){
+    int value = this.value + money.value;
+    return new Money(value);
+  }
+
+  public Money minus(Money money){
+    int value = this.value - money.value;
+    return new Money(value);
+  }
+}
--- StringUtils.java
+++ StringUtils.java
@@ -1,1 +1,34 @@
 // StringUtils
+
+class StringUtils {
+  static boolean containsChar(String word, char target){
+    for(int i = 0; i < word.length(); i++){
+      if(word.charAt(i) == target){
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  static String reverse(String word){
+    String reversed = "";
+    for(int i = word.length() - 1; i >= 0; i--){
+      reversed += word.charAt(i);
+
+    }
+    return reversed;
+  }
+
+  static String replaceAll(String word, char target, char replaceChar){
+    String replaced = "";
+    for(int i = 0; i < word.length(); i++){
+      if(word.charAt(i) == target){
+        replaced += replaceChar;
+      } else {
+        replaced += word.charAt(i);
+      }
+    }
+    return replaced;
+  }
+}
